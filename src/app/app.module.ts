a66a7c11d26925242ff45f9095970fce837f7a0f
SeilorMoon <nastya16.com@gmail.com>
2023-11-05 17:54:16 +0200

    it works without tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {PasswordCheckerComponent} from "./components/password-checker.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordCheckerComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/password-checker.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'password-checker',
+  templateUrl: './password-checker.component.html',
+  styleUrls: ['./password-checker.component.scss'],
+})
+export class PasswordCheckerComponent implements OnInit {
+  private uppercase = new RegExp(`(?=.*[A-Za-z])`);
+  private numbers = new RegExp(`(?=.*[0-9])`);
+  private symbols = new RegExp(`(?=.*[!@#$%]+$)`);
+  passwordCheck!: FormControl;
+
+  ngOnInit() {
+    this.passwordCheck = new FormControl('');
+  }
+
+  firstLineRule(password: string): string {
+    if (this.uppercase.test(password) && this.numbers.test(password) && this.symbols.test(password)) {
+      return '#54B435';
+    }
+    if (password.length < 3) {
+      return '#B9B4C7';
+    } else if (password.length < 8) {
+      return '#900C3F';
+    } else if (this.uppercase.test(password) && this.numbers.test(password)) {
+      return '#FF8D29';
+    } else {
+      return '#B9B4C7';
+    }
+  }
+
+  secondLineRule(password: string): string {
+    if (this.uppercase.test(password) && this.numbers.test(password) && this.symbols.test(password)) {
+      return '#54B435';
+    } else if (this.uppercase.test(password) && this.numbers.test(password)) {
+      return '#FF8D29';
+    } else {
+      return '#B9B4C7';
+    }
+  }
+
+  thirdLineRule(password: string): string {
+    if (this.uppercase.test(password) && this.numbers.test(password) && this.symbols.test(password)) {
+      return '#54B435';
+    } else {
+      return '#B9B4C7';
+    }
+
+  }
+
+}
