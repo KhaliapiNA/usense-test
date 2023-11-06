fecf6467ced5481c51c063830f195e9c68e7585f
SeilorMoon <nastya16.com@gmail.com>
2023-11-06 10:20:41 +0200

    new changes are in password-checker.component.ts for better solution

--- src/app/components/password-checker/password-checker.component.ts
+++ src/app/components/password-checker/password-checker.component.ts
@@ -10,41 +10,49 @@ export class PasswordCheckerComponent implements OnInit {
   private uppercase = new RegExp(`(?=.*[A-Za-z])`);
   private numbers = new RegExp(`(?=.*[0-9])`);
   private symbols = new RegExp(`(?=.*[!@#$%]+$)`);
+  private grey: string = '#B9B4C7';
+  private red: string = '#900C3F';
+  private yellow: string = '#FF8D29';
+  private green: string = '#54B435';
   passwordCheck!: FormControl;
 
   ngOnInit() {
     this.passwordCheck = new FormControl('');
   }
 
-  firstLineRule(password: string): string {
-    if (this.uppercase.test(password) && this.numbers.test(password) && this.symbols.test(password)) {
-      return '#54B435';
+   firstLineRule(password: string): string {
+    if (this.CheckGreenLine(password)) {
+      return this.green;
     } else if (password.length < 8 && (this.uppercase.test(password) || this.numbers.test(password) || this.symbols.test(password))) {
-      return '#900C3F';
+      return this.red;
     } else if (this.uppercase.test(password) && this.numbers.test(password)) {
-      return '#FF8D29';
+      return this.yellow;
     } else {
-      return '#B9B4C7';
+      return this.grey;
     }
   }
 
   secondLineRule(password: string): string {
-    if (this.uppercase.test(password) && this.numbers.test(password) && this.symbols.test(password)) {
-      return '#54B435';
+    if (this.CheckGreenLine(password)) {
+      return this.green;
     } else if (this.uppercase.test(password) && this.numbers.test(password) && password.length >= 8) {
-      return '#FF8D29';
+      return this.yellow;
     } else {
-      return '#B9B4C7';
+      return this.grey;
     }
   }
 
   thirdLineRule(password: string): string {
-    if (this.uppercase.test(password) && this.numbers.test(password) && this.symbols.test(password)) {
-      return '#54B435';
+    if (this.CheckGreenLine(password)) {
+      return this.green;
     } else {
-      return '#B9B4C7';
+      return this.grey;
     }
-
   }
 
+  CheckGreenLine(password: string): any {
+    if (this.uppercase.test(password) && this.numbers.test(password) && this.symbols.test(password)) {
+      return true;
+    }
+  }
 }
